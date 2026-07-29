2647ad4a6f3a61b4591910c2623482bf5b6fc9b5
Tien Nguyen <nguyenvantienthuanan12b3@gmail.com>
2026-07-29 22:32:28 +0700

    Update day3_matrix_multiplication.py

--- week01-02-vectors-matrices/day3_matrix_multiplication.py
+++ week01-02-vectors-matrices/day3_matrix_multiplication.py
@@ -22,3 +22,22 @@ print("\nS @ R (xoay rồi phóng to) =\n", combined)
 # Áp dụng lên vector v = (1, 0)
 v = np.array([1, 0])
 print("\nKết quả áp dụng lên v=(1,0):", combined @ v)
+
+
+# Bài tập buổi 3:
+# ex_1
+A_ex1 = np.array([[2, 1],
+                [0, 3]])
+B_ex1 = np.array([[1, 0],
+                [2, 1]])
+print("A_ex1 @ B_ex1 =\n", A_ex1 @ B_ex1)  
+print("B_ex1 @ A_ex1 =\n", B_ex1 @ A_ex1)  
+
+
+A2 = np.array([[1,2],[0,1]])
+B3 = np.array([[3,0],[1,2]])
+print("Bài luyện 1:", A2 @ B3)
+
+C2 = np.array([[2,-1],[3,0]])
+D3 = np.array([[1,4],[-2,1]])
+print("Bài luyện 2:", C2 @ D3)
